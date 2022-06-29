660341826a08fc33e910e72c4e48ed8b9c7951a5
Yakshit Jain <yakshit@criodo.com>
2022-06-29 14:25:15 +0550

    first commit

---
+++ src/main/java/com/crio/codingame/App.java
@@ -0,0 +1,16 @@
+package com.crio.codingame;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class App {
+    // To run the application  ./gradlew run --args="INPUT_FILE=input.txt"
+    public static void main(String[] args) {
+    }
+}
