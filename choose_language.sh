a24c76e0214832bbfe30d6538f229f888c413675
Yakshit Jain <yakshit@criodo.com>
2022-08-10 19:34:45 +0550

    updated changes - 2022-08-10 19:14:45

--- choose_language.sh
+++ choose_language.sh
@@ -10,10 +10,12 @@ select opt in Java Python; do
   case $opt in
     Java)
     python3 setup.py ./__CRIO__/metadata.json JAVA
+    rm -rf ./python
       break
       ;;
     Python)
     python3 setup.py ./__CRIO__/metadata.json PYTHON
+    rm -rf ./java
       break
       ;;
     *) 
